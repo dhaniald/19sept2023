865312fec3395647834e4d5a04ad02ecbb7e939c
Gaurav <gtiwari95@gmail.com>
2023-09-19 18:09:05 +0530

    v1

---
+++ src/app/page.js
@@ -0,0 +1,21 @@
+      //1. import area
+
+      import Footer from "@/component/Footer";
+      import Header from "@/component/Header";
+      import Main from "@/component/Main";
+
+      
+
+      //2. function defenation area
+      export default function Home() {
+        return (
+        <> 
+
+      <Header />
+      <Main />
+      <Footer />
+
+        </>
+        )
+      }
+      //export area
